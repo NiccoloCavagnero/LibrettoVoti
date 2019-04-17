ff99b332e0d256380791531a57371a4087558565
neikos <neikos@mbp-di-niccolo.station>
2019-04-17 17:09:58 +0200

    blabla

---
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.libretto;
+
+import java.util.*;
+
+public class Libretto {
+	
+	protected List<Voto> listaVoti;
+	
+	public Libretto() {
+		listaVoti = new LinkedList<Voto>();
+	}
+
+}
